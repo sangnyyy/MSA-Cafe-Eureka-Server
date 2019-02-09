a35dcb8b48187c5be6640e9f3329e9397c4d2d51
SML <dltkdals2202@gmail.com>
2019-02-10 01:27:47 +0900

    feat : eureka 서버 관련 설정
    Signed-off-by: SML <dltkdals2202@gmail.com>

--- src/main/java/me/sml/eureka/Application.java
+++ src/main/java/me/sml/eureka/Application.java
@@ -2,7 +2,10 @@ package me.sml.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
